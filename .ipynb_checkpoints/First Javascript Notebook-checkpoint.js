40146f856536c0e6de7f15c517eb2f16e77a0df3
Taylor Jaymes <tj4653a@gmail.com>
2023-12-25 15:52:27 -0800

    number 1

---
+++ .ipynb_checkpoints/First Javascript Notebook-checkpoint.js
@@ -0,0 +1,65 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "id": "228bd02f-d416-428d-be48-fda1dd4766d5",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "application/javascript": [
+       "console.log(\"hello\");\n",
+       "var x = 5;\n",
+       "var y = 6;\n",
+       "var z = x + y;\n",
+       "element.text(z)\n"
+      ],
+      "text/plain": [
+       "<IPython.core.display.Javascript object>"
+      ]
+     },
+     "metadata": {},
+     "output_type": "display_data"
+    }
+   ],
+   "source": [
+    "%%js\n",
+    "\n",
+    "var x = 4\n",
+    "var y = 5\n",
+    "var z = x + y\n",
+    "element.text(z)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "c046fc3f-c8d4-49c4-968a-270667eb8112",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}

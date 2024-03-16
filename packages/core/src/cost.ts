5c924627e8931e12684724231179d75f6b5579b7
Taylor Jaymes <tj4653a@gmail.com>
2024-03-16 12:21:59 -0700

    Addint unit tests

--- packages/core/src/cost.ts
+++ packages/core/src/cost.ts
@@ -1,13 +1,16 @@
-export function calculateCost(storage: number) {
-  
+//  New Rate Calculation
+//  const rate2 = storage * 20; // 20 per unit
+
+
+
+//  Default Rate Calculation
+//  The calculateCost function takes a storage parameter and returns
 //  if a user wants to store 10 or fewer notes, we’ll charge them 
 //  $4 per note. For 11 to 100 notes, we’ll charge $2 and any more 
 //  than 100 is $1 per note. Since Stripe expects us to provide the 
 //  amount in pennies (the currency’s smallest unit) we multiply 
 //  the result by 100.
-
-
-//  const rate2 = storage * 20; // 20 per unit
-    const rate = storage <= 20 ? 10 : storage <= 30 ? 10 : 5;
-    return rate * storage * 100;
-  }
+export function calculateCost(storage: number) {
+  const rate = storage <= 10 ? 4 : storage <= 100 ? 2 : 1;
+  return rate * storage * 100;
+}
